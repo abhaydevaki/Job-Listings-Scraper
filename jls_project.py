0191e9bcfbe4050b4c3e8e270f595c2489013c4f
abhaydevaki <abhaydevaki@gmail.com>
2024-10-08 00:01:29 +0530

    Refining Code

--- jls_project.py
+++ jls_project.py
@@ -3,50 +3,20 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
 from selenium.common.exceptions import NoSuchElementException
-# import os
 
-
-# webdriver.Chrome()
-# options = webdriver.ChromeOptions()
-# options.add_argument('--ignore-certificate-errors')
-# options.add_argument('--ignore-ssl-errors')
-# driver = webdriver.Chrome(options=options)
-
-# def main():
-#     required_role, required_location = user_requirements() 
-#     find_job_listings(required_role, required_location)
-
-# def user_requirements(): 
-    # global required_location
 required_role = input("What is the role you are looking for: ")
-if required_role == "":
-    required_role = "python developer"
-    # os.environ['required_role']=required_role
 required_location = input("What is the location you are looking for: ")
-if required_location == "":
-    required_location = "delhi"
-        # os.environ['required_location']=required_location
-
-    # return required_role, required_location
-
-
-# def for_importing():
-#     return required_location
 
 driver = webdriver.Chrome()
 
-# def find_job_listings(required_role, required_location):
 file_no = 0
 no_of_elems = 0
 
 
-# try:
 for _ in range(2):
     required_role_linkedin = required_role.replace(" ", "%20")
     driver.get(f"https://www.linkedin.com/jobs/search/?geoId=102713980&keywords={required_role_linkedin}&origin=JOBS_HOME_SEARCH_BUTTON&refresh=true")
-    # [15940:13120:0716/013427.758:ERROR:socket_manager.cc(141)] Failed to resolve address for stun.l.google.com., errorcode: -105
     time.sleep(4)
-
 
     try:
         element1 = driver.find_element(By.CLASS_NAME, "third-party-join__gsi-btn-container")
@@ -72,7 +42,6 @@ for _ in range(2):
     
     for _ in range(5):
         more_button_linkedin.click()
-        # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
         driver.execute_script("window.scrollBy(0, 1500);")
         time.sleep(4)
 
@@ -87,36 +56,9 @@ for _ in range(2):
             file_no += 1
     break
 
-# except IndexError:
-#     pass
-
-# except NoSuchElementException:
-#     driver.get(f"https://www.linkedin.com/jobs/search/?geoId=102713980&keywords={required_role_linkedin}&origin=JOBS_HOME_SEARCH_BUTTON&refresh=true")
-#     time.sleep(5)
-#     driver.implicitly_wait(5)
-#     for _ in range(6):
-#         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-#         time.sleep(4)
-#     driver.implicitly_wait(5)
-#     more_button_linkedin = driver.find_element(By.CLASS_NAME, "infinite-scroller__show-more-button")
-    
-#     for _ in range(5):
-#         more_button_linkedin.click()
-#         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-#         time.sleep(4)
-
-#     elems = driver.find_elements(By.CLASS_NAME , "base-search-card__info")
-#     no_of_elems += len(elems)
-
-#     for elem in elems:
-#         linkedin_data = elem.get_attribute("outerHTML")
-
-#         with open(f"jls_linkedin_database/{required_role}_{file_no}.html", "w", encoding="utf-8") as f:
-#             f.write(linkedin_data)
-#             file_no += 1
 
 try:
-    for i in range(10):
+    for i in range(3):
         required_role_indeed = required_role.replace(" ", "+")
         driver.get(f"https://in.indeed.com/jobs?q={required_role_indeed}&l={required_location}&start={10*i}")
         time.sleep(5)
@@ -140,12 +82,11 @@ try:
     cross_button_indeed = driver.find_element(By.CLASS_NAME, "CloseButton")
     cross_button_indeed.click()
     
-    for _ in range(9):
+    for _ in range(3):
         time.sleep(5)
         more_button = driver.find_element(By.CSS_SELECTOR, '[data-test="load-more"]')
         more_button.click()
-
-    
+  
     elems = driver.find_elements(By.CLASS_NAME, "JobCard_jobCardContent__X81Ew")
     no_of_elems += len(elems)
 
@@ -164,8 +105,13 @@ time.sleep(2)
 print(f"{no_of_elems} found")
 driver.close()
 
-# if __name__ == "__main__":
-#     main()
+
+
+
+
+
+
+
 
 # <a aria-label="Python Developer"></a>
 # <div <span class="job-card-container__primary-description"
